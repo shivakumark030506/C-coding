5d51236c1a37e8ef8b2c0e2f7c71963a7063f9d9
K.Shiva Kumar <mr.mammu.official@gmail.com>
2026-04-08 13:52:54 +0530

    highest,lowest,middle value.c

--- highest,lowest,middle value.c
+++ highest,lowest,middle value.c
@@ -1,65 +1,57 @@
+// Online C compiler to run C program online
 #include <stdio.h>
+
 void main()
 {
-    int a,b,c;
-    printf("Enter the first value:\n");
+    int a;
+    int b;
+    int c;
+    printf(" enter 1st value :\n");
     scanf("%d",&a);
-     printf("Enter the second value:\n");
+    printf(" enter 2nd value :\n");
     scanf("%d",&b);
-     printf("Enter the third value:\n");
+    printf(" enter 3rd value :\n");
     scanf("%d",&c);
-    if(a<b<c)
+    if (a>b && a>c)
     {
-        printf("%d is the highest value\n",c);
-        printf("%d is the middle value\n",b);
-        printf("%d is the lowest value\n",a);
+        printf("%d id the biggest \n",a);
+        if (b>c)
+        {
+            printf("%d is the middle \n",b);
+            printf("%d is the lowest \n",c);
+        }
+        else
+        {
+            printf("%d is the middle \n",c);
+            printf("%d is the lowest \n",b);
+        }
     }
-    else if(b<a<c)
+    else if (b>a && b>c)
     {
-        printf("%d is the highest value\n",b);
-        printf("%d is the middle value\n",a);
-        printf("%d is the smallest value\n",c);
+        printf("%d id the biggest \n",b);
+        if (a>c)
+        {
+            printf("%d is the middle \n",a);
+            printf("%d is the lowest \n",c);
+        }
+        else
+        {
+            printf("%d is the middle \n",c);
+            printf("%d is the lowest \n",a);
+        }
     }
-    else if(c<b<a)
+    else if (c>b && c>a)
     {
-        printf("%d is the highest value\n",c);
-        printf("%d is the middle value\n",b);
-        printf("%d is the smallest value\n",a);
+        printf("%d id the biggest \n",c);
+        if (b>a)
+        {
+            printf("%d is the middle \n",b);
+            printf("%d is the lowest \n",a);
+        }
+        else
+        {
+            printf("%d is the middle \n",a);
+            printf("%d is the lowest \n",b);
+        }
     }
-    else if(a<c<b)
-    {
-        printf("%d is the highest value\n",a);
-        printf("%d is the middle value\n",c);
-        printf("%d is the smallest value\n",b);
-    }
-    else if(b<c<a)
-    {
-        printf("%d is the highest value\n",b);
-        printf("%d is the middle value\n",c);
-        printf("%d is the smallest value\n",a);
-    }
-    else if(c<a<b)
-    {
-        printf("%d is the highest value\n",c);
-        printf("%d is the middle value\n",a);
-        printf("%d is the smallest value\n",b);
-    }
-    else if(a>b>c)
-    {
-        printf("%d is the highest value\n",a);
-        printf("%d is the middle value\n",b);
-        printf("%d is the smallest value\n",c);
-    }
-    else if(b>c>a)
-     {
-        printf("%d is the highest value\n",b);
-        printf("%d is the middle value\n",c);
-        printf("%d is the smallest value\n",a);
-     }
-     else if(c>b>a)
-      {
-        printf("%d is the highest value\n",c);
-        printf("%d is the middle value\n",b);
-        printf("%d is the smallest value\n",a);
-      }
 }
